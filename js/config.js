98b36bf1446b527d99f6d3214a34d71a6de6600e
rathee-rahul <rahulrathee811@gmail.com>
2026-05-27 18:19:28 +0530

    Add APK download link

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ window.REACH_CONFIG = {
   SUPABASE_URL: "https://eqocgylkivhkyqeqggff.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_lbYd1fgfuiXh3AeUWr2g2g_iZx_re1K",
   GOOGLE_WEB_CLIENT_ID: "27091859911-9426g7goi1o24acfi2o3uvo6jk5cjhfq.apps.googleusercontent.com",
-  APK_DRIVE_URL: ""
+  APK_DRIVE_URL: "https://drive.google.com/uc?export=download&id=1KM49uSIKv642Jp_a5Wdd_O1aho58Q2CQ"
 };
